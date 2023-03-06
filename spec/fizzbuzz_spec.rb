9e5d0f46e90e2648a5f2c11f3dfa07bbfdf329b9
umut <umut_baykan@hotmail.com>
2023-03-06 15:24:41 +0000

    tests for fizzbuzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,25 @@ describe 'fizzbuzz' do
   it 'returns fizz when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  
+  it 'returns buzz when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  
+  it 'returns fizzbuzz when passed 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  
+  it 'returns error when passed 0' do
+    expect(fizzbuzz(0)).to eq 'error'
+  end
+  
+  it 'returns buzz when passed -5' do
+    expect(fizzbuzz(-5)).to eq 'buzz'
+  end
+  
+  it 'returns fizzbuzz when passed 90' do
+    expect(fizzbuzz(90)).to eq 'fizzbuzz'
+  end
+  
 end
